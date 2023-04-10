6eeb9deeca66a4f88bfbe706d202466470cb548e
Erkin Bektenov <ericbek72@yahoo.com>
2023-04-10 23:20:41 +0600

    EC2 for Ansible-Master

--- AWS_tf_labs/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstates3backup"
-    key            = "tf-lab.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "state_lock"
-  }
-}
